3c0539d727a400d75366a009453fd3d218246b43
Vivek Shukla <viv3kshukla@gmail.com>
2017-05-23 22:34:42 +0530

    unsolved

---
+++ 5.cpp
@@ -0,0 +1,19 @@
+// http://codeforces.com/problemset/problem/118/A
+
+
+#include <iostream>
+#include <string>
+using namespace std;
+
+string remove_vowels(string s)
+{
+  for 
+}
+
+int main()
+{
+
+
+
+  return 0;
+}
