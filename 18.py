6d6d35d6ff8a9b2d34ecd890741f1cd973f35cbc
Vivek Shukla <viv3kshukla@gmail.com>
2017-05-26 23:38:36 +0530

    status

---
+++ 18.py
@@ -0,0 +1,17 @@
+# http://codeforces.com/problemset/problem/160/A
+
+
+n = int(input())
+
+li = list(map(int, input().split()))
+
+li.sort(reverse=True)
+
+count = 0
+
+for i in range(n):
+  if sum(li[:i+1]) > sum(li[i+1:]):
+    count = i+1
+    break
+
+print(count)
