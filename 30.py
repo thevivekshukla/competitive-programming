fddb75f04253107a59080ac8264a8587501352a6
Vivek Shukla <viv3kshukla@gmail.com>
2017-06-08 22:40:59 +0530

    solved

--- 30.py
+++ 30.py
@@ -1,5 +1,14 @@
 # http://codeforces.com/problemset/problem/337/A
 
+
+def _min(a, b):
+  if a<b:
+    return a
+  return b
+
+
+
+  
 n, m = tuple(map(int, input().split()))
 
 li = list(map(int, input().split()))
@@ -7,21 +16,13 @@ li = list(map(int, input().split()))
 
 li.sort()
 
-difference = []
+best = 9999999
 
-for i in range(1, m):
+for k in range(0, m-n+1):
+  best = _min(best, li[k+n-1]-li[k])
 
-  difference.append(abs(li[i-1]-li[i]))
+
+print(best)
 
 
-difference.sort()
-print(difference)
 
-s = 0
-
-if (n==2):
-  print(min(difference))
-else:
-  for i in range(n-1):
-    s += difference[i]
-  print(s)
