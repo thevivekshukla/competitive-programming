0aa2e39a1859c8bc7c78d9296acbf66c3d85aced
Vivek Shukla <viv3kshukla@gmail.com>
2018-03-15 01:23:34 +0530

    solved

---
+++ 54.py
@@ -0,0 +1,10 @@
+# https://www.hackerrank.com/challenges/nested-list/problem
+
+
+n = int(input())
+
+marksheet = [[input(), float(input())] for _ in range(n)]
+
+second_highest = sorted(list(set(marks for name, marks in marksheet)))[1]
+
+print('\n'.join([a for a,b in sorted(marksheet) if b == second_highest]))
